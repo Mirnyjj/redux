635eed85b97bd8e9f0da8889e1e4f3268cef5e11
Maksim <maks1733@gmail.com>
2023-12-21 21:59:43 +0400

    App tic-tac-toe using redux refresh

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,18 @@ import './App.css';
 import { AppLayout } from './AppLayout';
 import PropTypes from 'prop-types';
 import { store } from './store';
-import { useEffect} from 'react';
+import { useEffect, useState } from 'react';
 
 function App() {
-
+  const {buttonState} = store.getState();
+  const [refresh, setRefresh] = useState(buttonState)
+  
   useEffect(() => {
       store.subscribe(() => {
-          store.getState()
+          setRefresh(store.getState().buttonState)
     })
   }, [])
-
+  
   return (
     <>
       <AppLayout />
